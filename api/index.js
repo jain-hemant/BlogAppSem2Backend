ad823ad0975a455ad07f3283520f09e35cd7e891
jain-hemant <hemantjain1321@gmail.comcls>
2024-03-13 13:30:56 +0530

    testing deployment

--- api/index.js
+++ api/index.js
@@ -1,2 +1,12 @@
-import { start } from "./lib/app.js";
-start()
+// import { start } from "./lib/app.js";
+import express from 'express'
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('home page')
+})
+app.listen(5000, () => {
+    console.log('listening');
+})
+// start()
+
